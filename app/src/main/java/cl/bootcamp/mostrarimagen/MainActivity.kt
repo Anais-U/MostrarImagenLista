aa9eda3e399c9421470298583b4b06a22c612ebd
Anais Urra <terapiasancestralescl@gmail.com>
2024-09-12 14:37:36 -0300

    Actualizacion de imagenes e incorporacion de lista

--- app/src/main/java/cl/bootcamp/mostrarimagen/MainActivity.kt
+++ app/src/main/java/cl/bootcamp/mostrarimagen/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cl.bootcamp.mostrarimagen.ui.theme.MostrarImagenTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,43 +31,58 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     var isImageVisible by remember { mutableStateOf(false) }
-    var buttonText by remember { mutableStateOf("Mostrar Imagen") }
+    var buttonText by remember { mutableStateOf("Mostrar lista de Imagenes") }
+
+    val imageList = listOf(
+        R.drawable.bienvenidos,
+        R.drawable.feliz,
+        R.drawable.serfeliz,
+        R.drawable.felicidad,
+        R.drawable.lafelicidad
+    )
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(20.dp),
+            .padding(50.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Spacer(modifier = Modifier.height(50.dp))
+
         Text(
             text = "Bienvenido a mi aplicación",
+            fontSize = (20.sp),
             style = TextStyle(fontWeight = FontWeight.Bold),
             modifier = Modifier.padding(bottom = 8.dp)
         )
 
         Text(
             text = "Anais Urra",
+            fontSize = (18.sp),
             style = TextStyle(fontWeight = FontWeight.Normal),
             modifier = Modifier.padding(bottom = 16.dp)
         )
 
         Button(onClick = {
             isImageVisible = !isImageVisible
-            buttonText = if (isImageVisible) "Ocultar Imagen" else "Mostrar Imagen"
+            buttonText = if (isImageVisible) "Ocultar lista de Imagenes" else "Mostrar lista de Imagenes"
         }) {
             Text(text = buttonText)
         }
 
+        // LazyColumn para mostrar las imágenes
         if (isImageVisible) {
-            Image(
-                painter = painterResource(id = R.drawable.bienvenidos),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(top = 16.dp)
-                    .size(600.dp)
-            )
+            LazyColumn(modifier = Modifier.padding(top = 0.dp)) {
+                items(imageList.size) { index ->
+                    Image(
+                        painter = painterResource(id = imageList[index]),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(350.dp)
+                    )
+
+                }
+            }
         }
     }
 }
